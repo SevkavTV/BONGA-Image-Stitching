e7cacaba8be3cf9c5a2cb726b8f65783e79069f6
varchakov16 <varchakov@visualfabriq.com>
2022-05-30 14:46:52 +0300

    changed input file for panorama

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from models.camera import Camera
 from models.log import Log
 
 if __name__ == "__main__":
-    log_file = Log("flight-logs/fly_info.txt")
+    log_file = Log("flight-logs/fly_info_panorama.txt")
     camera = Camera(log_file)
     print(camera.images)
